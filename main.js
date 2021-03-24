acfc74a79ccadcd33b4f68710a65187689eb5860
HarrishTamilarasan <78077008+harrishtamil@users.noreply.github.com>
2021-03-24 15:31:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+var images=[
+    "https://i.pinimg.com/originals/87/b8/3b/87b83ba77a10a1d3313ed7872918524d.jpg","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5LnIBcXugMYwxclEKQjq_WMGq-S4PzG7z0Q&usqp=CAU","https://img1.pnghut.com/19/12/22/EQdkS9Nt8r/happiness-big-brother-caillou-television-show-smile-facial-expression.jpg","https://us.123rf.com/450wm/ankomando/ankomando1601/ankomando160100175/51337458-stock-vector-thumbs-up-baby.jpg?ver=6","https://i.pinimg.com/originals/db/f5/08/dbf508e27715f8ed3730bec349092789.jpg","https://i.pinimg.com/originals/d5/6c/d2/d56cd25436d0d3f56ad2f143750f7455.png"
+  ];
+  var i=0;
+  function nextslide(){
+  document.getElementById("photo").src = images[i]; i++;}
